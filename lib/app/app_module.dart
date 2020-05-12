f6cb8a6493184087fc73a99ceba858a39350bccf
Roberto Ribeiro <robertov82008@gmail.com>
2020-05-12 10:32:57 -0300

    Aula 4 - Salvando Tema no Dispositivo

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_gofast/app/app_controller.dart';
 import 'package:flutter_gofast/app/app_widget.dart';
+import 'package:flutter_gofast/app/core/interfaces/shared_repository_interface.dart';
+import 'package:flutter_gofast/app/core/repositories/shared_repository.dart';
 import 'package:flutter_gofast/app/modules/home/home_module.dart';
 import 'package:flutter_gofast/app/modules/splash/splash_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -8,6 +10,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
+        Bind<ISharedRepositoryInterface>((i) => SharedRepository()),
         Bind(
           (i) => AppController(),
         ),
